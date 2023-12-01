75180f4a16b8fd4b9460b40afb3c1d7082c20ec4
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-12-01 01:39:13 -0500

    Add code for DefaultHandler

---
+++ backend/DefaultHandler.js
@@ -0,0 +1,78 @@
+const AWS = require('aws-sdk');
+const redis = require("redis");
+
+exports.handler = async function (event, context) {
+  let connectionInfo;
+  let connectionId = event.requestContext.connectionId;
+
+  const callbackAPI = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint:
+      event.requestContext.domainName + '/' + event.requestContext.stage,
+  });
+
+  try {
+    connectionInfo = await callbackAPI
+      .getConnection({ConnectionId: event.requestContext.connectionId})
+      .promise();
+  } catch (e) {
+    console.log(e);
+  }
+
+  connectionInfo.connectionID = connectionId;
+
+  const redisClient = redis.createClient({
+    host: process.env.redisClusterAddr,
+    port: process.env.redisClusterPort,
+  });
+
+  let board;
+  try {
+    // Check if the board exists
+    board = await new Promise((resolve, reject) => {
+      redisClient.exists('board', (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+
+    // If the board doesn't exist, create a white board
+    if (!board) {
+
+      const whitePixel = "FFFFFF";
+      const totalPixels = 1000 * 1000;
+      board = whitePixel.repeat(totalPixels);
+
+      await new Promise((resolve, reject) => {
+        redisClient.set('board', board, (err, data) => {
+          if (err) reject(err);
+          else resolve(data);
+        });
+      });
+    }
+    else {
+      board = await new Promise((resolve, reject) => {
+        redisClient.get('board', (err, data) => {
+          if (err) reject(err);
+          else resolve(data);
+        });
+      });
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      message: `fail to conncet to board table with error: ${err}`
+    };
+  }
+
+  await callbackAPI
+    .postToConnection({
+      ConnectionId: event.requestContext.connectionId,
+      Data: JSON.stringify(board),
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+  };
+};
