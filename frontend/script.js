84bdf09dfe071c56ed8fb1140f7382594cfb7b5e
Feiran Huang <philipp.huang@mail.utoronto.ca>
2023-11-30 19:22:26 -0500

    fixed frontend parseing data

--- frontend/script.js
+++ frontend/script.js
@@ -8,134 +8,127 @@ var time;
 const publishableKey = "pk_test_a25vd2luZy1jb3JnaS00MS5jbGVyay5hY2NvdW50cy5kZXYk"; // <- Add Publishable Key here
 
 const startClerk = async () => {
-  const Clerk = window.Clerk;
+    const Clerk = window.Clerk;
 
-  try {
-    // Load Clerk environment and session if available
-    await Clerk.load();
+    try {
+        // Load Clerk environment and session if available
+        await Clerk.load();
 
-    const userButton = document.getElementById("user-button");
-    const authLinks = document.getElementById("auth-links");
+        const userButton = document.getElementById("user-button");
+        const authLinks = document.getElementById("auth-links");
 
-    Clerk.addListener(({ user }) => {
-      // Display links conditionally based on user state
-      authLinks.style.display = user ? "none" : "block";
-    });
+        Clerk.addListener(({ user }) => {
+            // Display links conditionally based on user state
+            authLinks.style.display = user ? "none" : "block";
+        });
 
-    if (Clerk.user) {
-      // Mount user button component
-      Clerk.mountUserButton(userButton);
-      userButton.style.margin = "auto";
+        if (Clerk.user) {
+            // Mount user button component
+            Clerk.mountUserButton(userButton);
+            userButton.style.margin = "auto";
+        }
+    } catch (err) {
+        console.error("Error starting Clerk: ", err);
     }
-  } catch (err) {
-    console.error("Error starting Clerk: ", err);
-  }
 };
 
 
 function initializeCanvas(data) {
-  for (let i = 0; i < 200; i++) {
-    for (let j = 0; j < 400; j++) {
-      const pixel = document.createElement('div');
-      pixel.classList.add('pixel');
-      pixel.classList.add('pixelBorder');
-      pixel.setAttribute('data-x', i);
-      pixel.setAttribute('data-y', j);
-      pixel.setAttribute('data-time', data[`${i},${j}`]?.time || 0);
-      pixel.style.backgroundColor = data[`${i},${j}`].color;
-      pixel.addEventListener('click', handlePixelClick);
-      canvas.appendChild(pixel);
+    data = data.Items;
+    for (let i = 0; i < 200; i++) {
+        for (let j = 0; j < 400; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.classList.add('pixelBorder');
+            pixel.setAttribute('data-x', i);
+            pixel.setAttribute('data-y', j);
+
+            const currentData = data.find(item => item.coordinate === `${i},${j}`);
+            pixel.setAttribute('data-time', currentData ? currentData.time : 0);
+            pixel.style.backgroundColor = currentData ? currentData.color : 'white';
+            pixel.addEventListener('click', handlePixelClick);
+            canvas.appendChild(pixel);
+        }
     }
-  }
-  doAction = updateCanvas;
+    doAction = updateCanvas;
 }
 
 function updateCanvas(data) {
-  const pixel = document.querySelector(`.pixel[data-x="${data.x}"][data-y="${data.y}"]`);
-  if (pixel.dataset.time >= data.time) return;
-  pixel.style.backgroundColor = data.color;
-  pixel.dataset.time = data.time;
+    const pixel = document.querySelector(`.pixel[data-x="${data.x}"][data-y="${data.y}"]`);
+    if (pixel.dataset.time >= data.time) return;
+    pixel.style.backgroundColor = data.color;
+    pixel.dataset.time = data.time;
 }
 
 function handlePixelClick(event) {
-  if (!Clerk.user) {
-    Clerk.openSignIn()
-    return;
-  }
-  if (time && Date.now() - time < 300000) {
-    dial.showModal();
-    return;
-  }
-  const pixel = event.target;
-  pixel.style.backgroundColor = currentColor;
-  const pixelData = {
-    x: pixel.dataset.x,
-    y: pixel.dataset.y,
-    color: currentColor,
-    user: Clerk.user.id,
-  };
-  socket.send(JSON.stringify({ "action": "sendmessage", "message": pixelData }));
-  time = Date.now();
+    if (!Clerk.user) {
+        Clerk.openSignIn()
+        return;
+    }
+    if (time && Date.now() - time < 300000) {
+        dial.showModal();
+        return;
+    }
+    const pixel = event.target;
+    pixel.style.backgroundColor = currentColor;
+    const pixelData = {
+        x: pixel.dataset.x,
+        y: pixel.dataset.y,
+        color: currentColor,
+        user: Clerk.user.id,
+    };
+    socket.send(JSON.stringify({ "action": "sendmessage", "message": pixelData }));
+    time = Date.now();
 }
 
 colorPicker.addEventListener('input', function () {
-  currentColor = colorPicker.value;
+    currentColor = colorPicker.value;
 });
 
 showBorder.addEventListener('change', function () {
-  const pixels = document.querySelectorAll('.pixel');
-  pixels.forEach(pixel => {
-    if (showBorder.checked) {
-      pixel.classList.add('pixelBorder');
-    } else {
-      pixel.classList.remove('pixelBorder');
-    }
-  });
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        if (showBorder.checked) {
+            pixel.classList.add('pixelBorder');
+        } else {
+            pixel.classList.remove('pixelBorder');
+        }
+    });
 });
 
 function connectWebSocket() {
-  socket = new WebSocket('wss://39h7sobph3.execute-api.us-east-2.amazonaws.com/production/');
+    socket = new WebSocket('wss://ksp6rp4kq5.execute-api.us-east-2.amazonaws.com/production/');
 
-  socket.addEventListener('open', () => {
-      initializeCanvas();
-  });
+    socket.addEventListener('open', (event) => {
+        socket.send(JSON.stringify({ "action": "initialize" }));
+    });
 
-  socket.addEventListener('error', () => {
-      const errorMessage = document.querySelector('.error-message');
-      errorMessage.style.display = 'block';
-      errorMessage.textContent = 'Failed to connect to server';
-  });
+    socket.addEventListener('error', () => {
+        const errorMessage = document.querySelector('.error-message');
+        errorMessage.style.display = 'block';
+        errorMessage.textContent = 'Failed to connect to server';
+    });
 
-  let doAction = initializeCanvas;
-
-  socket.addEventListener('message', (event) => {
-    const data = JSON.parse(event.data);
-    doAction(data);
-  });
-
-  const dummyData = {}
-  for (let i = 0; i < 200; i++) {
-    for (let j = 0; j < 400; j++) {
-      dummyData[`${i},${j}`] = {
-        color: 'white',
-      }
-    }
-  }
-  initializeCanvas(dummyData);
+    socket.addEventListener('message', (event) => {
+        if (!event.data) return;
+        const data = JSON.parse(event.data);
+        doAction(data);
+    });
 }
 
 
 (() => {
-  const script = document.createElement("script");
-  script.setAttribute("data-clerk-publishable-key", publishableKey);
-  script.async = true;
-  script.src = `https://cdn.jsdelivr.net/npm/@clerk/clerk-js@latest/dist/clerk.browser.js`;
-  script.crossOrigin = "anonymous";
-  script.addEventListener("load", startClerk);
-  script.addEventListener("error", () => {
-    document.getElementById("no-frontend-api-warning").hidden = false;
-  });
-  document.body.appendChild(script);
+    const script = document.createElement("script");
+    script.setAttribute("data-clerk-publishable-key", publishableKey);
+    script.async = true;
+    script.src = `https://cdn.jsdelivr.net/npm/@clerk/clerk-js@latest/dist/clerk.browser.js`;
+    script.crossOrigin = "anonymous";
+    script.addEventListener("load", startClerk);
+    script.addEventListener("error", () => {
+        document.getElementById("no-frontend-api-warning").hidden = false;
+    });
+    document.body.appendChild(script);
 })();
 
+var doAction = initializeCanvas;
 connectWebSocket();
